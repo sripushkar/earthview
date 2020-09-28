256dca61bb20b72555c1c24a1e6f7ced6a963281
Sri Julapally <sripushkar@gmail.com>
2020-09-28 01:20:55 -0400

    Update Weather.js

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -31,20 +31,29 @@ export default class Weather extends Component {
                             iconUrl: `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`,
                             temperature: this.kelvinToFahrenheit(data.main.temp),
                             minTemperature: this.kelvinToFahrenheit(data.main.temp_min),
-                            maxTemperature: this.kelvinToFahrenheit(data.main.temp_max)
+                            maxTemperature: this.kelvinToFahrenheit(data.main.temp_max),
+                            precipitation: `${data["weather"]["rain"]["1h"] * 100}%`,
+                            humidity: `${data.weather.main.humidity}%`
                         }
                     })
                 }) 
             .catch(error => console.log(error))
-        
-        
     }
 
     render() {
         
         return (
-            <div>
-                
+            <div className="card">
+                <header className="card-header">
+                    <p className="card-header-title">{this.state.weather.city}</p>
+                </header>
+                <div className="card-content">
+                    <div className="media">
+                        <div className="media-left">
+                            <img src={this.state.weather.iconUrl} alt="Weather Icon"/>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
