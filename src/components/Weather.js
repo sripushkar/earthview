019b763c5ecf7b86dbfc87d1410893e114293132
Sri Julapally <sripushkar@gmail.com>
2020-09-28 01:25:53 -0400

    moved api call to geolocate func

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -10,8 +10,28 @@ export default class Weather extends Component {
         navigator.geolocation.getCurrentPosition(position => {
             console.log("Latitude is :", position.coords.latitude);
             console.log("Longitude is :", position.coords.longitude);
-            
             this.setState({latitude: position.coords.latitude, longitude: position.coords.longitude})
+
+            const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${this.state.latitude}&lon=${this.state.longitude}&appid=${process.env.REACT_APP_WEATHER_API_KEY}`
+            fetch(weatherUrl)
+                .then(response => response.json())
+                    .then(data => {
+                        this.setState({
+                            weather: {
+                                city: data.name,
+                                main: data.weather[0].main,
+                                description: data.weather[0].description,
+                                iconUrl: `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`,
+                                temperature: this.kelvinToFahrenheit(data.main.temp),
+                                minTemperature: this.kelvinToFahrenheit(data.main.temp_min),
+                                maxTemperature: this.kelvinToFahrenheit(data.main.temp_max),
+                                precipitation: `${data["weather"]["rain"]["1h"] * 100}%`,
+                                humidity: `${data.weather.main.humidity}%`
+                            }
+                        })
+                    }) 
+                .catch(error => console.log(error))
+            console.log("Called API")
         });
     }
 
@@ -19,25 +39,6 @@ export default class Weather extends Component {
 
     //Fetches weather data from API
     componentDidUpdate(){
-        const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${this.state.latitude}&lon=${this.state.longitude}&appid=${process.env.REACT_APP_WEATHER_API_KEY}`
-        fetch(weatherUrl)
-            .then(response => response.json())
-                .then(data => {
-                    this.setState({
-                        weather: {
-                            city: data.name,
-                            main: data.weather[0].main,
-                            description: data.weather[0].description,
-                            iconUrl: `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`,
-                            temperature: this.kelvinToFahrenheit(data.main.temp),
-                            minTemperature: this.kelvinToFahrenheit(data.main.temp_min),
-                            maxTemperature: this.kelvinToFahrenheit(data.main.temp_max),
-                            precipitation: `${data["weather"]["rain"]["1h"] * 100}%`,
-                            humidity: `${data.weather.main.humidity}%`
-                        }
-                    })
-                }) 
-            .catch(error => console.log(error))
     }
 
     render() {
