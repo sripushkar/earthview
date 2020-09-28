51ec3babf5ad002735d34ee222b8db95563c0b52
Sri Julapally <sripushkar@gmail.com>
2020-09-27 21:16:16 -0400

    Create SettingsDropdown.js

---
+++ src/components/SettingsDropdown.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import '../Bulma.sass'
+
+export default class SettingsDropdown extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
