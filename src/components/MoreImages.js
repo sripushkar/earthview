8a2ba512d33fad03e5ddd06ba911575f268129a8
Sri Julapally <sripushkar@gmail.com>
2020-10-05 11:40:21 -0400

    add react component

---
+++ src/components/MoreImages.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import "../Bulma.sass"
+
+class MoreImages extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default MoreImages;
