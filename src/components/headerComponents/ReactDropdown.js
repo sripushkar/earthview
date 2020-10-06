9e9be31d8de7b81b1cb4b7268be1ee35b5b0d302
Sri Julapally <sripushkar@gmail.com>
2020-10-05 20:47:03 -0400

    Create ReactDropdown.js

---
+++ src/components/headerComponents/ReactDropdown.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import * as particles from "../../data/particlesConfig"
+import "../../Bulma.sass"
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngleDown } from "@fortawesome/free-solid-svg-icons";
+
+export default class particlesSettings extends Component {
+    state = {
+        collapsed: true,
+        selected: "Snow"
+    }
+
+    render() {
+        return (            
+        )
+    }
+}
